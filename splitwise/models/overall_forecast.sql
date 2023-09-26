a889c489f652e510bdff00a40263e9a2e2cd7478
ocamposfaria <ocamposfaria@gmail.com>
2023-09-26 17:24:59 -0300

    Adicionando segmentação de dados 'earning_category' na visão forecast

--- splitwise/models/overall_forecast.sql
+++ splitwise/models/overall_forecast.sql
@@ -1,6 +1,80 @@
 {{ config(materialized='view') }}
 
-SELECT 
+
+SELECT
+    *,
+    'ganhos + ganhos extra' as earning_category
+FROM 
+
+(SELECT 
+    `overall_costs`.`month` AS `month`,
+    `overall_costs`.`group` AS `group`,
+    `overall_costs`.`category` AS `category`,
+    `overall_costs`.`cost_juau` AS `cost_juau`,
+    `overall_costs`.`cost_lana` AS `cost_lana`,
+    'future_estimated' AS `future_mode`
+FROM
+    {{ref('overall_costs')}}
+WHERE
+    (CAST(CONCAT(`overall_costs`.`month`, '-01') AS DATE) <= (CURDATE() - INTERVAL 1 MONTH)) 
+UNION ALL SELECT 
+    `overall_costs_future_estimated`.`month` AS `month`,
+    `overall_costs_future_estimated`.`group` AS `group`,
+    `overall_costs_future_estimated`.`category` AS `category`,
+    `overall_costs_future_estimated`.`cost_juau` AS `cost_juau`,
+    `overall_costs_future_estimated`.`cost_lana` AS `cost_lana`,
+    'future_estimated' AS `future_mode`
+FROM
+    {{ref('overall_costs_future_estimated')}}
+UNION ALL SELECT 
+    `overall_costs`.`month` AS `month`,
+    `overall_costs`.`group` AS `group`,
+    `overall_costs`.`category` AS `category`,
+    `overall_costs`.`cost_juau` AS `cost_juau`,
+    `overall_costs`.`cost_lana` AS `cost_lana`,
+    'future_planned' AS `future_mode`
+FROM
+    {{ref('overall_costs')}}
+WHERE
+    (CAST(CONCAT(`overall_costs`.`month`, '-01') AS DATE) <= (CURDATE() - INTERVAL 1 MONTH)) 
+UNION ALL SELECT 
+    `overall_costs_future_planned`.`month` AS `month`,
+    `overall_costs_future_planned`.`group` AS `group`,
+    `overall_costs_future_planned`.`category` AS `category`,
+    `overall_costs_future_planned`.`cost_juau` AS `cost_juau`,
+    `overall_costs_future_planned`.`cost_lana` AS `cost_lana`,
+    'future_planned' AS `future_mode`
+FROM
+    {{ref('overall_costs_future_planned')}}
+UNION ALL SELECT 
+    `overall_costs`.`month` AS `month`,
+    `overall_costs`.`group` AS `group`,
+    `overall_costs`.`category` AS `category`,
+    `overall_costs`.`cost_juau` AS `cost_juau`,
+    `overall_costs`.`cost_lana` AS `cost_lana`,
+    'future_limited' AS `future_mode`
+FROM
+    {{ref('overall_costs')}}
+WHERE
+    (CAST(CONCAT(`overall_costs`.`month`, '-01') AS DATE) <= (CURDATE() - INTERVAL 1 MONTH)) 
+UNION ALL SELECT 
+    `overall_costs_future_limits`.`month` AS `month`,
+    `overall_costs_future_limits`.`group` AS `group`,
+    `overall_costs_future_limits`.`category` AS `category`,
+    `overall_costs_future_limits`.`cost_juau` AS `cost_juau`,
+    `overall_costs_future_limits`.`cost_lana` AS `cost_lana`,
+    'future_limited' AS `future_mode`
+FROM
+    {{ref('overall_costs_future_limits')}}) first_earning_category
+
+UNION ALL
+
+SELECT
+    *,
+    'ganhos' as earning_category
+FROM 
+
+(SELECT 
     `overall_costs`.`month` AS `month`,
     `overall_costs`.`group` AS `group`,
     `overall_costs`.`category` AS `category`,
@@ -60,3 +134,7 @@ UNION ALL SELECT
     'future_limited' AS `future_mode`
 FROM
     {{ref('overall_costs_future_limits')}}
+) second_earning_category
+
+WHERE
+    category <> 'ganhos extra'
