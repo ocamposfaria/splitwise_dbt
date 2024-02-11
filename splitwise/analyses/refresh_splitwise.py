f1694e0f2da6f782583fd23061b220bfe6dcf909
ocamposfaria <ocamposfaria@gmail.com>
2024-02-11 10:51:18 -0300

    Comentando viagem chapada

--- splitwise/analyses/refresh_splitwise.py
+++ splitwise/analyses/refresh_splitwise.py
@@ -109,19 +109,19 @@ def get_splitwise_expenses(group_id, group_name, limit):
     return df
 
 
-df_1 = get_splitwise_expenses(33823062, 'Nossa Residência', 100)
-df_2 = get_splitwise_expenses(34137144, 'VR', 100)
-df_3 = get_splitwise_expenses(35336773, 'just me', 100)
-df_4 = get_splitwise_expenses(40055224, 'apenas lana', 100)
-df_5 = get_splitwise_expenses(57014599, 'viagem chapada 2023', 100)
-#df_4 = get_splitwise_expenses(37823696, 'Harry Styles', 15)
-#df_4 = get_splitwise_expenses(40055224, 'apenas lana', 10)
-#df_5 = get_splitwise_expenses(32626795, 'Primavera Sound SP')
-#df_5 = get_splitwise_expenses(40780239, 'Black Mobly', 10)
-#df_5 = get_splitwise_expenses(39698610, 'Lollapalooza 2023', 15)
+df_1 = get_splitwise_expenses(33823062, 'Nossa Residência', 800)
+df_2 = get_splitwise_expenses(34137144, 'VR', 200)
+df_3 = get_splitwise_expenses(35336773, 'just me', 200)
+df_4 = get_splitwise_expenses(40055224, 'apenas lana', 200)
+# df_5 = get_splitwise_expenses(57014599, 'viagem chapada 2023', 100)
+# df_4 = get_splitwise_expenses(37823696, 'Harry Styles', 15)
+# df_4 = get_splitwise_expenses(40055224, 'apenas lana', 10)
+# df_5 = get_splitwise_expenses(32626795, 'Primavera Sound SP')
+# df_5 = get_splitwise_expenses(40780239, 'Black Mobly', 10)
+# df_5 = get_splitwise_expenses(39698610, 'Lollapalooza 2023', 15)
 
 
-df = pd.concat([df_1, df_2, df_3, df_4, df_5], ignore_index=True)
+df = pd.concat([df_1, df_2, df_3, df_4], ignore_index=True)
 
 # ### Getting month
 
