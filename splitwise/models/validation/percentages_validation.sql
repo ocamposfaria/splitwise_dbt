6bf49f326e3874360af0679790369c9cdd13e7a1
ocamposfaria <ocamposfaria@gmail.com>
2024-04-15 09:06:55 -0300

    Ignorando gastos específicos na validação de percentuais

--- splitwise/models/validation/percentages_validation.sql
+++ splitwise/models/validation/percentages_validation.sql
@@ -13,5 +13,11 @@ LEFT JOIN {{ref('monthly_percentages')}} mp ON mp.`month` = sf.`month`
 WHERE
     `source` = 'Nossa Residencia'
     and abs(sf.percentage_juau - mp.percentage_juau*100) >= 0.5
-    and sf.month > '2023-03'
+    and sf.month between '2023-04' and substring(curdate(), 1, 7)
     and sf.category <> 'compras'
+    and sf.expense_id not in (
+        '2355272455',
+        '2670473359',
+        '2842657545',
+        '2987075577'
+    )
